ad08ee5cf74e9cdecd4564dfd769a58e5312b388
Aethylia <fydrenak@gmail.com>
2021-01-26 21:28:40 +0000

    Wrote functions to find all valid polynomials.

--- sbox.js
+++ sbox.js
@@ -1,36 +1,95 @@
-function generate(irreducible_poly){
-    try{
-        p = parseInt(eval(irreducible_poly.replace(/x/g, '10')), 2);
-    } catch(err){
-        console.log('Irreducible Polynomial invalid');
-        return;
-    }
-
+function sbox(p){
     let t = new Uint32Array(256);
+
     for (let i = 0, x = 1; i < 256; i ++) {
         t[i] = x;
         x ^= (x << 1) ^ ((x >>> 7) * p);
     }
 
-    let Sbox = new Uint32Array(256);
-    Sbox[0] = 0x63;
+    let box = new Uint32Array(256);
+    box[0] = 0x63;
+
     for (let i = 0; i < 255; i ++) {
         let x = t[255 - i];
         x |= x << 8;
         x ^= (x >> 4) ^ (x >> 5) ^ (x >> 6) ^ (x >> 7);
-        Sbox[t[i]] = (x ^ 0x63) & 0xFF;
+        box[t[i]] = (x ^ 0x63) & 0xFF;
     }
 
-    return Sbox;
+    return box;
 }
 
 
-// Inverse of Sbox
-function inverse(sbox){
-    let InvSbox = new Uint32Array(256);
-    for (let i =0; i < 256; i++){
-        InvSbox[i] = sbox.indexOf(i);
+function entropy(sbox) {
+    const counts = new Uint8Array(256);
+    
+    for (const b of sbox) {
+        ++counts[b]
     }
 
-    return InvSbox;
+    let e = 0;
+    for (const c of counts) {
+        const p = c / sbox.length;
+
+        if (p > 0) {
+            e -= p * Math.log(p);
+        }
+    }
+
+    return e;
+}
+
+
+function getSetBits(num) {
+    const setBits = [];
+
+    let bitNum = 0;
+
+    while (num > 0) {
+        if (num & 1) {
+            setBits.push(bitNum);
+        }
+
+        num >>= 1;
+        ++bitNum;
+    }
+
+    return setBits;
+}
+
+function setBitsToPolyString(setBits) {
+    let s = "";
+
+    setBits.forEach((v, i) => {
+        if (v === 0) {
+            s += '1';
+        }
+        else if (v === 1) {
+            s += "x";
+        }
+        else {
+            s += `x**${v}`
+        }
+
+        if (i < setBits.length - 1) {
+            s += " + ";
+        }
+    });
+
+    return s;
+}
+
+//found experimentally by graphing entropy
+const threshold = 5.0; 
+
+for (let i = 0; i < 2**9; ++i) {
+    const s = sbox(i);
+    const e = entropy(s);
+
+    if (e > 5.0) {
+        const bits = getSetBits(i);
+        const polyString = setBitsToPolyString(bits);
+
+        console.log(polyString);
+    }
 }
